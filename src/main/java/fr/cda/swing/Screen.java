d383636637aa449c9e0ee9b17c4cc26144ea55a5
Guillaume Le Gall <glegall@greta-bretagne-sud.fr>
2025-10-14 15:53:18 +0200

    feat: adding class Screen

---
+++ src/main/java/fr/cda/swing/Screen.java
@@ -0,0 +1,29 @@
+package fr.cda.swing;
+
+import javax.swing.*;
+import java.awt.*;
+
+public abstract class Screen
+{
+    JFrame frame;
+
+    public Screen(final String title, final int width, final int height, final boolean autoShow)
+    {
+        frame = new JFrame(title);
+        frame.setSize(width, height);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Init();
+
+        if (autoShow)
+        {
+            Display();
+        }
+    }
+
+    public abstract void Init();
+
+    public void Display() {frame.setVisible(true);}
+
+    public void Hide() {frame.setVisible(false);}
+}
