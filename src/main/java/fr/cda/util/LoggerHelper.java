4c5a9b3214b2b634ecc181602837bb2aabd7053c
Guillaume Le Gall <glegall@greta-bretagne-sud.fr>
2025-10-14 16:20:43 +0200

    feat: adding LoggerHelper class

---
+++ src/main/java/fr/cda/util/LoggerHelper.java
@@ -0,0 +1,9 @@
+package fr.cda.util;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LoggerHelper
+{
+    public static final Logger Log = LogManager.getLogger(LoggerHelper.class);
+}
