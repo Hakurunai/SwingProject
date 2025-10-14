d635c663a83fcba4c67210de0e3c8cab9d021ce9
Guillaume Le Gall <glegall@greta-bretagne-sud.fr>
2025-10-14 16:11:11 +0200

    feat: adding main as entry point for the program

---
+++ src/main/java/fr/cda/main.java
@@ -0,0 +1,24 @@
+package fr.cda;
+
+public class main
+{
+    public final static boolean RUN_APP = true;
+
+    public static void RunApp()
+    {
+
+    }
+
+    public static void QuickTest()
+    {
+
+    }
+
+    public static void main(String[] args)
+    {
+        if (RUN_APP == true)
+            RunApp();
+        else
+            QuickTest();
+    }
+}
