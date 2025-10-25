80fc5a30870997a4f57084b148987bf1ea2961a4
Guillaume Le Gall <glegall@greta-bretagne-sud.fr>
2025-10-25 22:13:57 +0200

    feat : Creation of AsyncQueue class and his specialized child for Swing (main was updated to test it)

--- src/main/java/fr/cda/main.java
+++ src/main/java/fr/cda/main.java
@@ -1,10 +1,17 @@
 package fr.cda;
 
 import fr.cda.Site.Site;
+import fr.cda.async.AsyncQueue;
+
+import java.util.Random;
+import java.util.concurrent.*;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
 
 public class main
 {
-    public final static boolean RUN_APP = true;
+    public final static boolean RUN_APP = false;
 
     private static Site site;
 
@@ -15,8 +22,31 @@ public class main
 
     public static void QuickTest()
     {
+        AsyncQueue q = new AsyncQueue("QuickTest");
 
+        Supplier<Integer> supplier = () -> 10;
+        Consumer<Integer> consumer = (param) ->
+        {
+            for (int i = 1; i <= param; i++)
+            {
+                System.out.println(i);
+            }
+        };
+
+        q.SubmitAsyncOperation(supplier, consumer);
+        q.SubmitAsyncOperation(supplier, consumer);
+        q.SubmitAsyncOperation(supplier, consumer);
+        q.SubmitAsyncOperation(supplier, consumer);
+        q.Shutdown();
+        try
+        {
+            Thread.sleep(1000);
+        } catch (InterruptedException p_e)
+        {
+            throw new RuntimeException(p_e);
+        }
     }
+
 
     public static void main(String[] args)
     {
