9c5e95aea461d98264b9c71c3c996e02983a2c40
Guillaume Le Gall <glegall@greta-bretagne-sud.fr>
2025-11-01 22:12:37 +0100

    feat : add SerializerHelper class and new data in the .env/Config class

--- src/main/java/fr/cda/Config.java
+++ src/main/java/fr/cda/Config.java
@@ -9,11 +9,17 @@ public abstract class Config
     public static final String DB_ORDER_FILE_PATH;
     public static final String DB_PRODUCT_FILE_PATH;
 
+    public static final String REVIEW_ORDER_FILE_NAME;
+    public static final String REVIEW_ORDER_FILE_PATH;
+
     static
     {
         dotenv = Dotenv.load();
 
         DB_ORDER_FILE_PATH = dotenv.get("DB_ORDER_FILE_PATH");
         DB_PRODUCT_FILE_PATH = dotenv.get("DB_PRODUCT_FILE_PATH");
+
+        REVIEW_ORDER_FILE_PATH = dotenv.get("REVIEW_ORDER_FILE_PATH");
+        REVIEW_ORDER_FILE_NAME = dotenv.get("REVIEW_ORDER_FILE_NAME");
     }
 }
---
+++ src/main/java/fr/cda/util/SerializerHelper.java
@@ -0,0 +1,69 @@
+package fr.cda.util;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+/**
+ * Utility class for general serialisation necessity
+ */
+public abstract class SerializerHelper
+{
+    public static final String PATH_SEPARATOR = "/";
+
+    public static void SerializeToFile(final String content, final String path, final String fileName)
+    {
+        LoggerHelper.log.info("TRY : to serialize file {} at path {}", fileName, path);
+
+        if (!EnsureOrCreatePath(path))
+        {
+            LoggerHelper.log.error("FAIL : to ensure/create path : " + path);
+            return;
+        }
+
+        Path filePath = Path.of(path, fileName);
+
+        try
+        {
+            Files.writeString
+                    (
+                    filePath,
+                    content,
+                    StandardOpenOption.CREATE,
+                    StandardOpenOption.TRUNCATE_EXISTING
+                    );
+
+            LoggerHelper.log.info("SUCCESS : File written to : " + filePath.toAbsolutePath());
+        }
+        catch (IOException e)
+        {
+            LoggerHelper.log.error("FAIL : while writing file : " + filePath + " | " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Util method used to ensure that a path exist. If not, this method will generate it
+     * @param filePath The path you want to ensure his existence
+     * @return True in case of success, false if an issue occurred during the process
+     */
+    public static boolean EnsureOrCreatePath(final String filePath)
+    {
+        LoggerHelper.log.info("TRY to ensure/create path : " + filePath);
+        Path path = Paths.get(filePath);
+
+        try
+        {
+            Files.createDirectories(path);
+            LoggerHelper.log.info("Creation of path SUCCEED to path : " + filePath);
+            return true;
+
+        } catch (IOException e)
+        {
+            LoggerHelper.log.error("Creation of path FAIL to path : " + filePath);
+            return false;
+        }
+    }
+}
